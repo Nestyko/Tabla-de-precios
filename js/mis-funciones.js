a0b325ff2639bb97fdb4ea5862e448ba2d9da9a3
Nestor Luis Tobon <nltobon@gmail.com>
2015-10-29 23:39:17 -0430

    Agregar funcion para los checkbox, pero tengo problemas con el index y la seleccion de los checkboxes

--- js/mis-funciones.js
+++ js/mis-funciones.js
@@ -1,13 +1,36 @@
 
 var precios = [700000, 500000, 1000000],
 		quantity = $('.quantity'),
-		sub_total = $('.sub-total');
+		sub_total = $('.sub-total'),
+        //====================================================
+        checkboxes = $('input[type="checkbox"]');//Me supongo que esto me debe generar un arreglo como los de arriba
+        //====================================================
 
 //Esta version es mas sencilla porque practicamente todo lo que hay que hacer es añadir un listener a cualquier input que este activo
 
 quantity.on('input', function(){
-	console.log(this);
+	//console.log(this);
 	getSubtotal(this, $(this).data("index")); //aqui aprovechamos que HTML5 permite crear atributos personalizados para tomar el indice con JQUery y pasarlo a la funcion getSubtotal
+});
+
+checkboxes.change(function(){
+    var checkb = $(this);
+    //================================================================================
+    var index = $(this).data("index"); //Aqui es donde necesito ayuda
+    //================================================================================
+    index = 1;
+    console.log(this);
+    console.log("index: " + index);
+    if(checkb.is(":checked")){
+       if(quantity.eq(index).val() < 1){
+            quantity.eq(index).val(1);
+            getSubtotal(quantity.get(index),index);
+        }
+        sub_total.eq(index).removeClass("tachado");
+    }else{
+        sub_total.eq(index).addClass("tachado");
+    }
+
 });
 
 function getSubtotal(element, output){
