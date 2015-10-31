abceb8f41b896f0ceb8cdcc00303e52b68ddf897
Nestor Luis Tobon <nltobon@gmail.com>
2015-10-31 00:40:46 -0430

    Replanteado todo el codigo, para que funcione con los cambios en las filas de la tabla. Gracias especiales a codashazbot

--- js/mis-funciones.js
+++ js/mis-funciones.js
@@ -1,46 +1,32 @@
 
-var precios = [700000, 500000, 1000000],
-		quantity = $('.quantity'),
-		sub_total = $('.sub-total'),
-        //====================================================
-        checkboxes = $('input[type="checkbox"]');//Me supongo que esto me debe generar un arreglo como los de arriba
-        //====================================================
 
-//Esta version es mas sencilla porque practicamente todo lo que hay que hacer es añadir un listener a cualquier input que este activo
+function init(){
+    var rows = $("tr").find(".quantity");
+    rows.prop("disabled", true);
+}
 
-quantity.on('input', function(){
-	//console.log(this);
-	getSubtotal(this, $(this).data("index")); //aqui aprovechamos que HTML5 permite crear atributos personalizados para tomar el indice con JQUery y pasarlo a la funcion getSubtotal
-});
-
-checkboxes.change(function(){
-    var checkb = $(this);
-    //================================================================================
-    var index = $(this).data("index"); //Aqui es donde necesito ayuda
-    //================================================================================
-    index = 1;
-    console.log(this);
-    console.log("index: " + index);
-    if(checkb.is(":checked")){
-       if(quantity.eq(index).val() < 1){
-            quantity.eq(index).val(1);
-            getSubtotal(quantity.get(index),index);
+function main(){
+    init();
+    $("tr").change(function (event){
+        var row = $(this);
+        var checkbox = row.find("input:checkbox");
+        var quant = row.find(".quantity");
+        var sub_tot = row.find(".sub-total");
+        var price = (row.find(".price").html());
+        if(checkbox.is(":checked")){
+            quant.prop("disabled", false);
+            if(quant.val() < 1){
+                quant.val(1);
+            }
+            sub_tot.removeClass("tachado");
+        }else{
+            quant.prop("disabled", true);
+            sub_tot.addClass("tachado");
         }
-        sub_total.eq(index).removeClass("tachado");
-        quantity.eq(index).prop("disabled", false);
-    }else{
-        sub_total.eq(index).addClass("tachado");
-        quantity.eq(index).prop("disabled", true);
-    }
+        sub_tot.html(quant.val()*price);
+    });
 
-});
 
-function getSubtotal(element, output){
-	//console.log('caller is: ' + element + 'index is:' + output); //test para debuggear
-	sub_total[output].innerHTML = element.value * precios[output];
 }
 
-
-function activateCheck(elem){
-
-}
+$(document).ready(main());
